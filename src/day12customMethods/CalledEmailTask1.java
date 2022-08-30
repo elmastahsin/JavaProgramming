3be5c83b2f2455939304452ed3e9809d4a86cbea
Tyson <tyson@cydeo.com>
2022-08-30 08:02:53 +0300

    JavaProgramming

---
+++ src/day12customMethods/CalledEmailTask1.java
@@ -0,0 +1,22 @@
+package day12customMethods;
+
+import java.util.Scanner;
+
+public class CalledEmailTask1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your email: ");
+
+
+    }
+}
+/*
+*   2. Create a class calledEmailTask1.
+       Assume that email address is constructed by person's first name and followed by an underscore and last name.
+
+        Write a program that can swap first name with last name in the email (Saperated by an underscore).
+        If the email doesn't contain an underscore print the given input email.
+
+            Ex:
+                input: mike_tyson@gmail.com
+                output: tyson_mike@gmail.com*/
---
+++ src/day12customMethods/FormatFullName.java
@@ -0,0 +1,31 @@
+package day12customMethods;
+
+import java.util.Scanner;
+
+public class FormatFullName {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your first name :");
+        String firstName = input.next();
+        System.out.println("Enter your last name :");
+        String lastName = input.next();
+        input.close();
+
+        firstName = firstName.substring(0, 1).toUpperCase() + firstName.substring(1).toLowerCase();
+        lastName = lastName.substring(0, 1).toUpperCase() + lastName.substring(1).toLowerCase();
+        String fullName = firstName + " " + lastName;
+         /* String fullName = firstName.substring(0, 1).toUpperCase() + firstName.substring(1).toLowerCase()
+                + " " + lastName.substring(0, 1).toUpperCase() + lastName.substring(1).toLowerCase();*/
+
+        System.out.println("fullName = " + fullName);
+    }
+}
+/*
+* 1. Write a program that asks user to enter first and last names, and then prints the full name in regular format (first character in upper case)
+
+	                    input:
+	                        firstName = "cyDEo"
+	                        lastName = "SCHOOL";
+
+	                    output:
+	                        Cydeo School*/
