fe4967b4a9b8f82bb1916f9ef73fa560e1dbf1db
Tyson <tyson@cydeo.com>
2022-08-31 11:44:45 +0300

    JavaProgramming

--- src/day14_forLoop/MethodOverLoadingPractice.java
+++ src/day14_forLoop/MethodOverLoadingPractice.java
@@ -5,6 +5,9 @@ public class MethodOverLoadingPractice {
         System.out.println(sum(10,45));
         System.out.println(sum(23,234,43));
         System.out.println(sum(32,12,56,687));
+        System.out.println(sum(32.23, 23.32));
+        System.out.println(sum(23.2,123.32,324.34));
+        System.out.println(sum(12.34,4364.463,345.435,453.45));
     }
 
 
@@ -19,8 +22,15 @@ public class MethodOverLoadingPractice {
     public static int sum (int n1,int n2,int n4,int n3){
         return sum(n1,n2,n3) + n4;
     }
-
-
+    public static double sum (double n1, double n2){
+        return n1+n2;
+    }
+    public static double sum (double n1, double n2,double n3){
+        return sum(n1,n2)+ n3;
+    }
+    public static double sum (double n1, double n2,double n3,double n4){
+        return sum(n1,n2,n3)+ n4;
+    }
 
 
 
