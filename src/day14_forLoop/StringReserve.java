6b4a412585681ba092b59f386a85a9e66e9df445
Tyson <tyson@cydeo.com>
2022-08-31 17:42:35 +0300

    JavaProgramming

--- src/day14_forLoop/StringReserve.java
+++ src/day14_forLoop/StringReserve.java
@@ -20,9 +20,10 @@ public class StringReserve {
     }
 
     public static String reverse(String str) {
-        String reverse = "";
-        for (int i = str.length() - 1; i >= 0; i--) {
-            reverse += str.charAt(i);
+        String reverse = "";// to contain all the characters of the given string in reversed order
+
+        for (int i = str.length() - 1; i >= 0; i--) {// i: index number of the given string starting from last index to index 0
+            reverse += str.charAt(i);// to get each character of the string starting from last index to index zero
         }
         return reverse;
     }
