c4e212d92a298d02e3742f91977558e2a10b8614
Tyson <tyson@cydeo.com>
2022-08-31 18:20:54 +0300

    JavaProgramming

---
+++ src/day15_whileLoop/MaximumNumber.java
@@ -0,0 +1,40 @@
+package day15_whileLoop;
+
+import java.util.Scanner;
+
+public class MaximumNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int max = -2147483648;
+        // write a program that can ask the user enter to enter a number for five times,  print the maximum number
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Enter " + (i + 1) + ". number :");
+            int a = input.nextInt();
+            System.out.println(i + 1 + ". number:" + a);
+            if (a > max) {
+                max = a;
+            }
+
+        }
+        System.out.println("max = " + max);
+
+        System.out.println("*****************************");
+
+        // write a program that can ask the user enter to enter a number for five times,  print the minimum number
+
+
+        int min = 0;
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Enter " + (i + 1) + ". number :");
+            int a = input.nextInt();
+            System.out.println(i + 1 + ". number:" + a);
+            if (i == 0) {
+                min = a;
+            }
+            if (a < min) {
+                min = a;
+            }
+            System.out.println("min = " + min);
+        }    }
+}
