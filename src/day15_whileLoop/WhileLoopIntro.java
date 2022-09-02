1dc56124b7972a578665a68615af4480ead2c96a
Tyson <tyson@cydeo.com>
2022-09-02 13:33:14 +0300

    JavaProgramming

---
+++ src/day15_whileLoop/WhileLoopIntro.java
@@ -0,0 +1,22 @@
+package day15_whileLoop;
+
+public class WhileLoopIntro {
+    public static void main(String[] args) {
+        if (true){
+            System.out.println("Hello Cydeo");
+        }
+
+        System.out.println("***********************");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("I Love Java");
+            
+        }
+
+        System.out.println("********************");
+
+
+        while (true){
+            System.out.println("I love Java");
+        }
+    }
+}
