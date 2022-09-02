f5fc4220aca542727e2a4d393e5e9935309c98ae
Tyson <tyson@cydeo.com>
2022-09-02 13:18:37 +0300

    JavaProgramming

--- src/day15_whileLoop/ContinueStatement.java
+++ src/day15_whileLoop/ContinueStatement.java
@@ -35,5 +35,23 @@ public class ContinueStatement {
             System.out.println("Hello");
             
         }
+
+        System.out.println("*********************");
+
+        for (int i = 50; i < 101; i+=2) {
+            System.out.print(i + " ");
+
+        }
+
+        System.out.println();
+        for (int i = 50; i < 101 ; i++) {
+            if (i%2!=0){
+                continue;
+            }
+            System.out.print(i +" ");
+
+        }
+
+
     }
 }
