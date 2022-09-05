f90a0f886afcda562befedc5d5f529ab10430b77
Tyson <tyson@cydeo.com>
2022-09-05 14:07:40 +0300

    JavaProgramming

--- src/day16_nestedLoop/CydeoLogin.java
+++ src/day16_nestedLoop/CydeoLogin.java
@@ -45,3 +45,17 @@ public class CydeoLogin {
         }
     }
 }
+/*
+* 1. Create a class named CydeoLogIn. you are writing a code for the log-in function of the Cydeo Application, assume that your credentials are:
+                    username: Cydeo
+                    password: WoodenSpoon
+
+        Ask the user to enter their credentials.
+                If credentials are matched, your program should print "Logged in."
+
+                If the credentials are not matched, the program should allow
+                the user to have three attempts to enter correct credentials
+
+                if all three attempts are failed, then print "Your account is lucked."
+
+*/
--- src/day16_nestedLoop/FrequencyOfChar.java
+++ src/day16_nestedLoop/FrequencyOfChar.java
@@ -1,4 +1,32 @@
 package day16_nestedLoop;
 
+import java.util.Scanner;
+
 public class FrequencyOfChar {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string :");
+        String str = input.next();
+        System.out.println("Enter a char :");
+        String ch = input.next();
+        int charCount = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ch.charAt(0)) {
+                charCount++;
+            }
+
+        }
+        System.out.println(str);
+        System.out.println(ch);
+        System.out.println(charCount);
+
+    }
 }
+/* 2. write a program that can return the frequency of a character from a string
+        	Ex:
+        		str = "aaabbbbccccc"
+        		ch = 'c'
+
+		     output:5
+		     	*/
