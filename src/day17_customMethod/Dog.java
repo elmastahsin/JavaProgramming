acf2262d16b2744e70797a2f3b90668b53732fc2
Tyson <tyson@cydeo.com>
2022-09-07 16:28:35 +0300

    JavaProgramming

---
+++ src/day17_customMethod/Dog.java
@@ -0,0 +1,17 @@
+package day17_customMethod;
+
+public class Dog {
+    public String name;// instance variables
+    public String breed;
+    public int age;
+    public String size;
+    public String color;
+
+    public void eat(){
+        System.out.println(name + "is eating");
+    }
+    public void drink(){
+        System.out.println(name + " is drinking water");
+    }
+
+}
---
+++ src/day17_customMethod/Test.java
@@ -0,0 +1,4 @@
+package day17_customMethod;
+
+public class Test  {
+}
