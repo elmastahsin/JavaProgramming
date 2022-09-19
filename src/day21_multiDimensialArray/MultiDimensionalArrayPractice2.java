e496824efc54fcc3553a3a061713e848c8926998
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-09-19 11:35:05 +0300

    JavaProgramming

---
+++ src/day21_multiDimensialArray/MultiDimensionalArrayPractice2.java
@@ -0,0 +1,40 @@
+package day21_multiDimensialArray;
+
+import java.util.Arrays;
+
+public class MultiDimensionalArrayPractice2 {
+
+    public static void main(String[] args) {
+        int[][] arr2D1 = {{1, 2}, {3, 4, 5}};
+        int[][] arr2D2 = {{6, 7, 8, 9}, {10, 11}, {12, 13, 14}};
+        int[][] arr2D3 = {{15, 16, 17, 18}};
+
+        // int [][][] arr3D  = {arr2D1,arr2D2,arr2D3};
+        //                       0         1              0            1           2                0
+        int[][][] arr3D = {{{1, 2}, {3, 4, 5}}, {{6, 7, 8, 9}, {10, 11}, {12, 13, 14}}, {{15, 16, 17, 18}}};  //index: 0 ~ 2
+        //      index:              0                                1                                 2
+
+
+        System.out.println(Arrays.deepToString(arr3D));
+
+        System.out.println(Arrays.deepToString(arr3D[1]));
+
+        System.out.println(Arrays.toString(arr3D[1][2]));
+        System.out.println(Arrays.toString(arr3D[1][0]));
+        System.out.println(arr3D[1][2][2]);
+        System.out.println(arr3D[1][2][1]);
+
+
+        System.out.println("--------------------");
+
+        for (int i = 0; i < arr3D.length; i++) {
+            for (int j = 0; j < arr3D[i].length; j++) {
+                for (int k = 0; k < arr3D[i][j].length; k++) {
+                    System.out.println(arr3D[i][j][k]);
+                }
+            }
+
+            }
+        }
+
+    }
