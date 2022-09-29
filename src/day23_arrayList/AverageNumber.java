95921562fe51ca4d91b29fea4fc89d3e55f29336
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-09-29 14:18:17 +0300

    JavaProgramming

--- src/day23_arrayList/AverageNumber.java
+++ src/day23_arrayList/AverageNumber.java
@@ -6,19 +6,21 @@ public class AverageNumber {
 
     public static void main(String[] args) {
 
-        ArrayList<Integer> list=new ArrayList<Integer>();
+        ArrayList<Integer> list = new ArrayList<>();
 
         list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        list.add(62);
+        list.add(70);
 
-        System.out.println();
-
-
-
-
-
-
-
-
-
+        System.out.println(list);
+        int sum = 0;
+        for (Integer each : list) {
+            sum += each;
+        }
+        double average = (double) sum / list.size();
+        System.out.println("average = " + average);
     }
 }
