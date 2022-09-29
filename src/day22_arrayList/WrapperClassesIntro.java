31837df04ca3901f866b71ecd5ffbc7249fbd3f7
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-09-29 11:22:17 +0300

    JavaProgramming

--- src/day22_arrayList/WrapperClassesIntro.java
+++ src/day22_arrayList/WrapperClassesIntro.java
@@ -1,8 +1,6 @@
 package day22_arrayList;
 
 
-import day17_customClass.Dog;
-
 public class WrapperClassesIntro {
     public static void main(String[] args) {
 
---
+++ src/day23_arrayList/AverageNumber.java
@@ -0,0 +1,24 @@
+package day23_arrayList;
+
+import java.util.ArrayList;
+
+public class AverageNumber {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list=new ArrayList<Integer>();
+
+        list.add(20);
+
+        System.out.println();
+
+
+
+
+
+
+
+
+
+    }
+}
