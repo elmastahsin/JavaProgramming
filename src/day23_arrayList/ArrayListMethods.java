85848354336f599bbc778416d0d622744d14d60e
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-09-29 19:34:10 +0300

    JavaProgramming

--- src/day23_arrayList/ArrayListMethods.java
+++ src/day23_arrayList/ArrayListMethods.java
@@ -36,15 +36,40 @@ public class ArrayListMethods {
         System.out.println("---------------------------");
 
         ArrayList<Integer> numbers = new ArrayList<>();
-        numbers.add(10); // 1
-        numbers.add(20); // 2
-        numbers.add(30); // 3
-        numbers.add(40); // 4
-        numbers.add(50); // 5
+        numbers.add(10); // 0
+        numbers.add(20); // 1
+        numbers.add(30); // 2
+        numbers.add(40); // 3
+        numbers.add(50); // 4
 
-        numbers.remove(Integer.valueOf(20));
-
+        boolean r1 = numbers.remove(Integer.valueOf(20));
         System.out.println(numbers);
+        System.out.println(r1);
+        numbers.clear();
+        System.out.println(numbers);
+        System.out.println(numbers.size());
+
+        System.out.println("------------------------");
+
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Vasyl"); // 0
+        names.add("Vasyl"); // 1
+        names.add("Sumeye"); //2
+        names.add("Sumeye");
+        names.add("Ali");
+        names.add("Sumeye");
+
+        System.out.println(names.indexOf("Vasyl"));
+        System.out.println(names.lastIndexOf("Vasyl"));
+
+        System.out.println(names.lastIndexOf("Sumeye"));
+
+        boolean hasMuhtar = names.contains("Muhtar");
+        boolean hasAli = names.contains("Ali");
+
+        System.out.println("hasMuhtar = " + hasMuhtar);
+        System.out.println("hasAli = " + hasAli);
 
     }
 }
---
+++ src/day23_arrayList/RemoveDuplicates.java
@@ -0,0 +1,33 @@
+package day23_arrayList;
+
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.ArrayList;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Vasyl"); // 0
+        names.add("Vasyl"); // 1
+        names.add("Sumeye"); //2
+        names.add("Sumeye");
+        names.add("Ali");
+        names.add("Sumeye");
+
+
+        System.out.println(names); //[Vasyl, Vasyl, Sumeye, Sumeye, Ali, Sumeye]
+
+        ArrayList<String> nonDup = new ArrayList<>();
+
+        for (String each : names) {
+           if (nonDup.contains(each)){
+               continue;
+           }
+           nonDup.add(each);
+        }
+       names = nonDup;
+        System.out.println(names);//[Vasyl, Sumeye, Ali]
+
+        System.out.println(nonDup);
+    }
+}
