b28d598f0c2ea440f2c303c2035eaa537dda5f48
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-10-06 07:37:55 +0300

    JavaProgramming

--- src/day25_constructors/Rectangle.java
+++ src/day25_constructors/Rectangle.java
@@ -7,16 +7,23 @@ public class Rectangle {
     public double area(){
         return length*width;
     }
-
+/*
 public void setInfo(double length, double width){
 this.width=width;
 this.length=length;
-}
+}*/
+
+    public Rectangle(double length, double width){
+        this.length = length;
+        this.width = width;
+
+    }
     @Override
     public String toString() {
         return "Rectangle{" +
                 "length=" + length +
                 ", width=" + width +
+                ", area=" + area() +
                 '}';
     }
 }
