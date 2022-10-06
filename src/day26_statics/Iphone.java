ee3afaace64b8fe871bb2053168f3edf506d029e
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-10-06 15:30:52 +0300

    JavaProgramming Day 26

---
+++ src/day26_statics/Iphone.java
@@ -0,0 +1,21 @@
+package day26_statics;
+
+public class Iphone {
+    public static String brand;
+    public String color;
+    public String model;
+    public static String OS;
+    public static String madeIn;
+
+
+}
+/*
+* Attributes;
+* brand
+* color
+* model
+* price
+* OS
+* madeIn
+
+* */
