c0868614c2eda9f12ac6558093de383d944fb395
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-10-06 15:45:14 +0300

    JavaProgramming Day 26

--- src/day26_statics/Iphone.java
+++ src/day26_statics/Iphone.java
@@ -1,21 +1,49 @@
 package day26_statics;
 
 public class Iphone {
-    public static String brand;
-    public String color;
+    public static String brand = "Apple";
     public String model;
-    public static String OS;
-    public static String madeIn;
+    public double price;
+    public String color;
+    public static String OS = "IOS";
+    public static String madeIn = "China";
+    public static boolean hasBattery = true;
+    public static boolean hasTouchScreen = true;
+    public static boolean isExpensiveToFix = true;
 
+    /*
+    public static void printPhoneInfo(){ // static only accepts statics
+        System.out.println("Model: " + model);
+        System.out.println("Color: " + color);
+    }
+    */
+
+    public Iphone(String model, double price, String color) {
+        this.model = model;
+        this.price = price;
+        this.color = color;
+    }
+
+    public void printPhoneInfo() { // instance methods accepts both static and none static
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Color: " + color);
+        System.out.println("Price: " + price);
+    }
+    public static void printOperatingSystem(){
+        System.out.println("Operating System: " + OS);
+    }
 
 }
 /*
-* Attributes;
-* brand
-* color
-* model
-* price
-* OS
-* madeIn
-
-* */
+ * Attributes;
+ * brand
+ * color
+ * model
+ * price
+ * OS
+ * madeIn
+ * hasBattery
+ * hasTouchScreen
+ * isEcpensivetoFix
+ * */
