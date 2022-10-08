ba77c33d047a3201268fe3875be31225b50a9493
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-10-08 15:21:38 +0300

    JavaProgramming Day 27

---
+++ src/day28_encapsulation/encapsulation/Student.java
@@ -0,0 +1,5 @@
+package day28_encapsulation.encapsulation;
+
+public class Student {
+ private int age ;
+}
---
+++ src/day28_encapsulation/encapsulation/Test.java
@@ -0,0 +1,11 @@
+package day28_encapsulation.encapsulation;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Student student1 = new Student();
+
+       // student1.age = -200;
+
+    }
+}
