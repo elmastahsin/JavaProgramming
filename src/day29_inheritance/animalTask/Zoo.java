c51349227a0da7352cb5be2c6d90993ad713da2c
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-10-12 17:11:52 +0300

    JavaProgramming Day 27

---
+++ src/day29_inheritance/animalTask/Zoo.java
@@ -0,0 +1,11 @@
+package day29_inheritance.animalTask;
+
+public class Zoo {
+    public static void main(String[] args) {
+
+        Dog dog1= new Dog();
+
+        System.out.println(dog1);
+
+    }
+}
