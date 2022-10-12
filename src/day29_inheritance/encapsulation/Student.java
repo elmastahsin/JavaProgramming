0b22c19e3476a215306de66af37b4dd4ef697e0f
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-10-12 14:54:44 +0300

    JavaProgramming Day 27

---
+++ src/day29_inheritance/encapsulation/Student.java
@@ -0,0 +1,32 @@
+package day29_inheritance.encapsulation;
+
+public class Student {
+    private String name;
+    private int age;
+    private char gender;
+    private String grade;
+    private String schoolName;
+
+
+
+
+}
+/*Warmup tasks:
+    1. Create a class named Student:
+            private variables:
+                name, age, gender, grade, schoolName
+
+            Encapsulate all the fileds (at leat encapsulate two fields manually)
+                    requirments:
+                        1. age can not be less than 5 or greater than 90
+                        2. gender should not be set to any chanarcter other than: 'M' and 'F'
+                        3. grade should not be set to any chanacters other than: 'A', 'B', 'C', 'D', and 'F'
+
+            Add a constructor that can set all teh fields when the object is created
+                        (requirments of fileds in the above must be applied)
+
+
+            Methods:
+                study()
+                toString()
+*/
