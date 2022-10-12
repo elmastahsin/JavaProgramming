d6169618a7c15e523cece5d97b8e4acbce963d8d
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-10-12 17:15:36 +0300

    JavaProgramming Day 29

--- src/day29_inheritance/animalTask/Zoo.java
+++ src/day29_inheritance/animalTask/Zoo.java
@@ -3,9 +3,16 @@ package day29_inheritance.animalTask;
 public class Zoo {
     public static void main(String[] args) {
 
-        Dog dog1= new Dog();
-
+        Dog dog1 = new Dog();
+        dog1.setInfo("Max", "Husky", 'F', "Small", "White", 4);
         System.out.println(dog1);
 
+        Cat cat1 = new Cat();
+        cat1.setInfo("Felicia", "Stray", 'F', "Small", "Black", 3);
+        System.out.println(cat1);
+
+        Tiger tiger1 = new Tiger();
+        tiger1.setInfo("Share Khan", "Bengal", 'M', "Large", "Orange", 5);
+        System.out.println(tiger1);
     }
 }
