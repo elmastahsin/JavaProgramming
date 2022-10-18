2af9b55666ecfad8ea7bb677b3fc4872d18aa7ea
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-10-18 22:39:18 +0300

    Java Programming Day 32 Final Keyword

---
+++ src/day32_finalKeyword/FinalKeyword.java
@@ -0,0 +1,44 @@
+package day32_finalKeyword;
+
+import java.time.LocalDate;
+
+class CydeoStudent {
+    public final void language() {
+        System.out.println("Java Programming");
+    }
+
+}
+
+
+public  class FinalKeyword extends CydeoStudent {
+    /*@Override
+    public void language() { // final method can never be overridden
+        System.out.println("Pyhton Programming");
+    }*/
+
+    public static void main(String[] args) {
+
+        final char gender = 'M';
+
+        System.out.println(gender);
+
+        // gender = 'F';
+
+        System.out.println(gender);
+
+
+        System.out.println("--------------------");
+
+        final LocalDate dateOfBirth = LocalDate.now();
+
+        System.out.println(dateOfBirth);
+
+        // dateOfBirth = dateOfBirth.plusYears(1);
+
+        System.out.println(dateOfBirth);
+
+        new CydeoStudent().language();
+        new FinalKeyword().language();
+
+    }
+}
