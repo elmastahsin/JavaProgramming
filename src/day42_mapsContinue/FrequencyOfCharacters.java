9b62cc9d0fe5e5d4dfaf6260a0af8f823b05bcd6
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-12-02 18:58:51 +0300

    JavaProgramming Day 42 Maps

---
+++ src/day42_mapsContinue/FrequencyOfCharacters.java
@@ -0,0 +1,41 @@
+package day42_mapsContinue;
+
+import java.text.CollationElementIterator;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FrequencyOfCharacters {
+    public static void main(String[] args) {
+
+        String str = "bbcccaaaaa";
+
+        Map<Character, Integer> map = new LinkedHashMap<>();
+
+        for (String each : str.split("")) {
+            int frequency = Collections.frequency(Arrays.asList(str.split("")), each);
+            //   System.out.println(each + " : " + frequency);
+            map.put(each.charAt(0), frequency);
+        }
+        System.out.println(map);
+
+
+    }
+}
+/*1. Write a program that can return the frequency of characters
+            Not: MUST use map
+
+                Ex: str = "bbcccaaaaa"
+
+                output:
+                    {b=2, c=3, a=5}
+
+	2. Write a program that can find the unique characters from a String
+	              Not: MUST use map
+	            Ex:
+	                    str = "aabcccdeeeef";
+
+	            output:
+	                    {b=1, d=1, f=1}
+*/
