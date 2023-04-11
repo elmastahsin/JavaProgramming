c0bee5cc31a94e6d8e74a85d8344240ac4a78aa3
Tahsin Elmas <111126837+elmastahsin@users.noreply.github.com>
2023-04-11 22:10:49 +0300

    Update HelloMETU.java
    Signed-off-by: Tahsin Elmas <111126837+elmastahsin@users.noreply.github.com>

--- src/day01_introToProgramming/HelloMETU.java
+++ src/day01_introToProgramming/HelloMETU.java
@@ -2,6 +2,7 @@ package day01_introToProgramming;
 
 public class HelloMETU {
     public static void main(String[] args) {
+        
         System.out.println("Hello METU");
         System.out.println();
         System.out.println("My name is Tahsin");
